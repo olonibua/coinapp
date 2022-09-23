d66b896c2c93e651926d455afe241d6a169235d4
tolulope55 <tolulopeolonibua@gmail.com>
2022-09-24 05:06:21 +1300

    first commit

--- src/App.js
+++ src/App.js
@@ -1,24 +1,38 @@
-import logo from './logo.svg';
-import './App.css';
+import React, { useEffect, useState } from 'react';
+import { Route, Routes } from 'react-router-dom';
+import Navbar from './components/Navbar';
+import { ThemeProvider } from './context/ThemeContext';
+import Home from './routes/Home';
+import axios from 'axios';
+import CoinPage from './routes/CoinPage';
+import Trending from './components/Trending';
+import Footer from './components/Footer';
 
 function App() {
+  const [coins, setCoins] = useState([]);
+
+  const url ='https://api.coingecko.com/api/v3/coins/markets?vs_currency=usd&order=market_cap_desc&per_page=10&page=1&sparkline=true';
+
+  useEffect(() => {
+    axios.get(url).then((response) => {
+      setCoins(response.data);
+      // console.log(response.data)
+    });
+  }, [url]);
+
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+    <ThemeProvider>
+        <Navbar />
+        <Routes>
+          {/* <Home /> */}
+          <Route path='/' element={<Home coins={coins} />} />
+          <Route path='/trending' element={<Trending />} />
+          <Route path='/coin/:coinId' element={<CoinPage />}>
+            <Route path=':coinId' />
+          </Route>
+        </Routes>
+        <Footer />
+    </ThemeProvider>
   );
 }
 
